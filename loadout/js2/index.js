061f6f74dd800e162b028d051383be9ace821262
Litu <58910604+LituDev@users.noreply.github.com>
2022-05-16 01:06:47 +0200

    feat(loadout): functionnal modal.

--- loadout/js2/index.js
+++ loadout/js2/index.js
@@ -56,9 +56,9 @@ class Cards {
     }
 
     renderCards() {
-        this.cards.forEach(card => {
+        this.cards.forEach((card, i) => {
             let data = `
-            <div class="card">
+            <div id="card-${i}" class="card">
                 <img class="info" src="./svgs/circle-info.svg">
                 <div class="info-bubble">
             `;
@@ -81,6 +81,88 @@ class Cards {
                 </div>
             </div>`
             this.cardParent.insertAdjacentHTML('beforeend', data);
+            document.getElementById(`card-${i}`).addEventListener('click', () => {
+                j.renderPlayerModal();
+            });
+        });
+    }
+}
+
+class Modal {
+    constructor() {
+        this.modalParent = document.getElementById('modal-parent');
+
+        this.players = [{
+            LABEL: 'Player 1',
+            HEALTH: '75', // health/100
+            LEVEL: '1',
+        }]
+    }
+
+    renderModal() {
+        this.modalParent.style.display = "flex";
+        let data = `
+        <div id="modal">
+            <img src="./svgs/circle-xmark.svg" id="close-modal">
+        </div>
+        `
+        this.modalParent.innerHTML = "";
+        this.modalParent.insertAdjacentHTML('beforeend', data);
+        document.getElementById("close-modal").addEventListener("click", () => this.modalParent.style.display = "none")
+    }
+
+    renderPlayerModal() {
+        this.renderModal();
+
+        let data = `
+        <div class="modal-left-side">
+            <div class="modal-list-filter">
+                <input type="text" placeholder="filter: Origin" class="filter-input">
+                <input type="text" placeholder="filter: Faction" class="filter-input">
+                <input type="text" placeholder="filter: Level" class="filter-input">
+            </div>
+            <div id="modal-list"></div>
+        </div>
+
+        <div class="modal-description">
+            <img class="img-horizontal-responsive" src="../assets/ui/browse.png">
+            <div class="horizontal-wrapper">
+                <div id="modal-info-1">Hey</div>
+                <div id="modal-info-2">Hey</div>
+            </div>
+        </div>
+        `
+        document.getElementById("modal").insertAdjacentHTML('beforeend', data);
+
+        this.renderPlayerList();
+    }
+
+    renderPlayerList() {
+        const listParent = document.getElementById('modal-list');
+        this.players.forEach(player => {
+            let data = `
+            <div class="modal-list-node">
+                <img class="img-horizontal-responsive" src="../assets/ui/browse.png">
+                <div class="node-text">${player.LABEL}</div>
+                <div class="node-stats">
+                    <div class="horizontal-wrapper">
+                        <div class="node-text">Health</div>
+                        <div class="bar-container">
+                            <div class="bar" style="width: ${player.HEALTH}%"></div>
+                        </div>
+                    </div>
+                    <div class="horizontal-wrapper">
+                        <div class="node-text">Level</div>
+                        <div class="bar-container">
+                            <div class="bar" style="width: ${75}%"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="node-text">Skills: None</div>
+            </div>
+            `
+
+            listParent.insertAdjacentHTML('beforeend', data);
         });
     }
 }
@@ -88,8 +170,4 @@ class Cards {
 const i = new Cards();
 i.renderCards();
 
-class Modal {
-    constructor() {
-        
-    }
-}
+const j = new Modal();
