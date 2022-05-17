5e453606e1f17fa3102e6ad6e1050d7a22485e1e
Felix Ruzzoli <felix.ruzzoli@lrx.gmbh>
2022-05-17 23:43:47 +0200

    add api endpoint for troop data loading

--- doserve/servepages.py
+++ doserve/servepages.py
@@ -3,8 +3,11 @@ import json
 import flask
 from string import Template
 import sys,os
-sys.path.append(os.getcwd())
+sys.path.append('..')
 from doserve.common.character import Character
+from storage import Storage
+
+db = Storage('battles.sqlite')
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -19,28 +22,40 @@ def prepareTroopData(generation, char):
     templateData = char.toObject()
     templateData['faction'] = str(templateData['faction']).replace('Faction.', '')
     # cast all skill to str
-    to_str = lambda x: str(x)
-    templateData['skills'] = map(to_str, templateData['skills'])
-    templateData['skillString'] = ', '.join(templateData['skills']) if templateData['skills'] else 'None'
+    to_str = lambda x: str(x['name'])
+    templateData['skills'] = templateData['skills']
+    templateData['skillString'] = ', '.join(map(to_str, templateData['skills']))
     templateData = create_nav_links(char, generation, templateData)
     return templateData
 
 def prepareSummaryData(battle_data):
     templateData = {}
     templateData['battleId'] = battle_data['battleId']
+    walletOne = None
+    walletTwo = None
     for wallet, index in battle_data['playerMap'].items():
-        templateData['player{}'.format(index)] = wallet
+        if index == 0:
+            walletOne = wallet
+        elif index == 1:
+            walletTwo = wallet
+        templateData['player{}'.format(index)] = '<a href="/player/{}">{}</a>'.format(wallet, wallet)
     templateData['winner'] = battle_data['winner']
     templateData['turnCount'] = battle_data['turnCount']
     troopsByPlayer = {
-        templateData['player0']: [],
-        templateData['player1']: []
+        walletOne: [],
+        walletTwo: []
     }
     for char_data in battle_data['allCharacters']:
-        troopsByPlayer[char_data['ownerWallet']].append(char_data['tokenId'])
+        tokenId = char_data['tokenId']
+        generation = char_data['generation'] if 'generation' in char_data else 1
+        troopsByPlayer[char_data['ownerWallet']].append((generation, tokenId))
 
-    templateData['player0Troops'] = ', '.join(troopsByPlayer[templateData['player0']])
-    templateData['player1Troops'] = ', '.join(troopsByPlayer[templateData['player1']])
+    # wrap all troops in a href by using map-reduce
+    player0TroopLinks = map(lambda x: '<a href="/troops/{}/{}">{}</a>'.format(x[0], x[1], x[1]), troopsByPlayer[walletOne])
+    player1TroopLinks = map(lambda x: '<a href="/troops/{}/{}">{}</a>'.format(x[0], x[1], x[1]), troopsByPlayer[walletTwo])
+
+    templateData['player0Troops'] = ', '.join(player0TroopLinks)
+    templateData['player1Troops'] = ', '.join(player1TroopLinks)
     return templateData
 
 def create_nav_links(char, generation, templateData):
@@ -55,11 +70,16 @@ def create_nav_links(char, generation, templateData):
     return templateData
 
 
-@app.route('/troops/<int:generation>/<int:tokenId>', methods=['GET'])
-def troop_detail(generation, tokenId):
+@app.route('/api/troops/<int:generation>/<int:tokenId>', methods=['GET'])
+def troop_detail_json(generation, tokenId):
     char = Character('NoOwner', tokenId)
     templateData = prepareTroopData(generation, char)
-    return render('troops_detail', templateData)
+    return templateData
+
+
+@app.route('/troops/<int:generation>/<int:tokenId>', methods=['GET'])
+def troop_detail(generation, tokenId):
+    return render('troops_detail', troop_detail_json(generation, tokenId))
 
 
 @app.route('/battle-summary/<int:battleId>', methods=['GET'])
@@ -70,5 +90,13 @@ def battle_summary(battleId):
         battle_data = json.load(file)
         templateData = prepareSummaryData(battle_data)
     return render('battle_summary', templateData)
+
+
+@app.route('/player/<player_wallet>', methods=['GET'])
+def player_details(player_wallet):
+    player_data = db.findPlayer(player_wallet)
+    return render('player_details', player_data)
+
+
 
 app.run()
