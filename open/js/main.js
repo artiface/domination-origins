6aa64cddfd4660d96aa2c71cbfee539c9ac7f972
LituDev <xlitu035@gmail.com>
2022-08-01 23:47:20 +0200

    feat(open): added depedencies.

--- open/js/main.js
+++ open/js/main.js
@@ -1,7 +1,7 @@
 import CSS from "../css/style.css"
 import * as THREE from 'three';
 import * as TWEEN from '@tweenjs/tween.js';
-import { getContract } from "./chainload";
+import { signer, provider, connect, sendTokens, onTokensReceived, txlink } from "./chainload";
 import { ethers } from "ethers";
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
@@ -305,3 +305,35 @@ document.getElementById("exit-container").style.display = "flex";
 
 const starterPack = new Starter("starter")
 const boosterPack = new Booster("booster")
+
+
+async function onReceived(tx_hash, token_list) {
+    console.log("Received", token_list, "different tokens");
+    const txAnchor = `https://mumbai.polygonscan.com/tx/${tx_hash}`// txlink(tx_hash);
+    //result.innerHTML += `<p>Received ${token_list.length} tokens</p>`;
+    //result.innerHTML += `<p>TX: ${txAnchor}</p><ul>`;
+    for (let i = 0; i < token_list.length; i++) {
+        //const token = token_list[i];
+        //result.innerHTML += `<li>${token.amount}x ${token.id}</li>`;
+    }
+    //result.innerHTML += "</ul>";
+}
+
+try {
+    const sendStarterKit = async function() {
+        //result.innerHTML = "Sending starter kit...";
+        const amount = 1//parseInt(document.getElementById("token_amount").value);
+        // send starter kit tokens to the token receiver..
+        const erc1155_token_receiver = '0x37FD34a131b07ce495f7D16275B6dc4Ed1Bbd8C5';
+        const tokenId = 1;
+        const tx = await sendTokens(erc1155_token_receiver, tokenId, amount);
+        //result.innerHTML = "<p>Sent " + amount + " tokens to " + erc1155_token_receiver + " with tx " + txlink(tx.hash) + "</p>";
+    };
+    
+    //const sendButton = document.getElementById("send");
+    //sendButton.addEventListener("click", sendStarterKit);
+    onTokensReceived(onReceived);
+}
+catch (e) {
+    alert("Error during the transaction: " + e);
+}
